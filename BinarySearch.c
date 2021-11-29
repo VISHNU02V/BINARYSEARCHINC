c8a77bb1f12c20d5863620b738c1418903092de8
Vishnu V <84262256+VISHNU02V@users.noreply.github.com>
2021-11-29 22:09:40 +0530

    Update BinarySearch.c

--- BinarySearch.c
+++ BinarySearch.c
@@ -1,12 +1,12 @@
 // To perform binary search
 
 #include <Stdio.h>
-void sort(int a[],int n){
+void sort(int a[],int n){                        //SORTING.......
     int i=1;
     for(i=1;i<n;i++){
         if(a[i-1]>a[i]){
             for(i=1;i<n;i++){
-                if(a[i-1]>a[i]){
+                if(a[i-1]>a[i]){                        
                     a[i-1]=a[i-1]-a[i];
                     a[i]=a[i-1]+a[i];
                     a[i-1]=a[i]-a[i-1];
@@ -27,7 +27,7 @@ void main(){
         printf("enter the length of the array::");
         scanf("%d",&n);
         printf("enter the elements into array:\n");
-        for(i=0;i<n;i++){
+        for(i=0;i<n;i++){                                   // Intialising array
             scanf("%d",&a[i]);
         }
         printf("THE ARRAY\n  [");
@@ -35,12 +35,12 @@ void main(){
             printf(" %d,",a[i]);
         }
         printf("]");
-        printf("\nTHE SORTED ARRAY\n  [");
+        printf("\nTHE SORTED ARRAY\n  [");                  // Before performing binary search sorting must be done
         for(i=n;i>0;i--){
             sort(a,n);
         }
         for(i=0;i<n;i++){
-            printf(" %d,",a[i]);
+            printf(" %d,",a[i]);                            // sorted
         }
         printf("]\n\n\n\n# binary search starting ..............\n\n");
         printf("enter the element to be searched::");
@@ -48,7 +48,7 @@ void main(){
         low=0;
         high=n-1;
         printf("\n~~~~~~~~~~~~~~ Output ...............\n");
-        while (low<=high){
+        while (low<=high){                                  // Binary search happening.....
             mid=(int)(low+high)/2;
             if(b>a[mid]){
                 low=mid+1;
@@ -62,7 +62,7 @@ void main(){
             }
         }
         if (flag==0){
-            printf("\n  element not found");
+            printf("\n  element not found");                
         }
         else{
                 printf("\n  element ,%d, found at %d th position",b,mid+1);
